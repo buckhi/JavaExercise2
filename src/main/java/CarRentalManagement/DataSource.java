92a2bba3ed7eed02055c695534b81eec5364d7ae
nbhai <blingangel98@gmail.com>
2022-04-01 09:18:44 +0700

    asd

--- src/main/java/CarRentalManagement/DataSource.java
+++ src/main/java/CarRentalManagement/DataSource.java
@@ -1,17 +1,59 @@
 package CarRentalManagement;
 
+import java.sql.*;
+
 public class DataSource {
-    public static final String DB_NAME = "car.db";
+    public static final String DB_NAME = "car_rental.db";
     public static final String CONNECTION_STRING = "jdbc:sqlite:C:\\Users\\nbhai\\Desktop\\projects\\NguyenBacHaiExercise2\\" + DB_NAME;
 
     public static final String START_DATE = "startDate";
     public static final String END_DATE = "endDate";
-    public static final String DATE_PATTERN = "dd-MM-yyyy";
 
-    public static final String ID_CODE = "carCode";
-    public static final String BRAND = "brand";
-    public static final String MODEL = "model";
-    public static final String SEAT_NUMBER = "seatNo";
-    public static final String LICENSE_PLATE = "plate";
+    public static final String TABLE_CAR = "car_rental";
+    public static final String ID_CODE = "ID";
+    public static final String ID_CODE1 = "IDC";
+    public static final String BRAND = "Brand";
+    public static final String MODEL = "Model";
+    public static final String SEAT_NUMBER = "Seat";
+    public static final String LICENSE_PLATE = "Plate";
+
+    public static final String RENTAL_TABLE = "rentalManagement";
+    public static final String CLIENT_NAME = "fullname";
+
+    public static final String QUERY_RENTAL_INFO = "select " + TABLE_CAR + "." + ID_CODE + " from " + TABLE_CAR + " inner join " +
+            RENTAL_TABLE + " on " + TABLE_CAR + "." + ID_CODE + " = " + RENTAL_TABLE + "." + ID_CODE1 + ";";
+
+    private Connection connection;
+    private PreparedStatement queryRentalInfo;
+
+//    public boolean open() {
+//        try {
+//            connection = DriverManager.getConnection(CONNECTION_STRING);
+//            queryRentalInfo = connection.prepareStatement(QUERY_RENTAL_INFO);
+//            return true;
+//        } catch (SQLException exception) {
+//            System.out.println("couldn't connect to the data base " + exception.getMessage());
+//            return false;
+//        }
+//    }
+//
+//    public void close() {
+//        try {
+//            if (queryRentalInfo != null) {
+//                queryRentalInfo.close();
+//            }
+//        } catch (SQLException exception) {
+//            System.out.println("couldn't close connection " + exception.getMessage());
+//        }
+//    }
+
+
+    public static void insertRental(Statement statement, String name, String startDate, String endDate, String ID) throws SQLException {
+        statement.execute("insert into " + RENTAL_TABLE + " (" + CLIENT_NAME + ", " +
+                START_DATE + ", " + END_DATE + ", " + ID_CODE + ") " +
+                "values('" + name + "', '" + startDate + "', '" + endDate + "', '" + ID +"')");
+
+    }
+
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,71 @@
+import CarRentalManagement.DataSource;
+
+import javax.xml.crypto.Data;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.*;
+import java.util.Scanner;
+
+import static CarRentalManagement.DataSource.*;
+
 public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+//        Scanner scanner = new Scanner(new File("C:\\Users\\nbhai\\Desktop\\car_rental.csv"));
+//        scanner.useDelimiter(" , ");
+//
+//        while (scanner.hasNext()) {
+//            System.out.print(scanner.next());
+//        }
+//        scanner.close();
+
+        try {
+            Connection connection = DriverManager.getConnection(CONNECTION_STRING);
+            Statement statement = connection.createStatement();
+//            statement.execute("create table if not exists " + RENTAL_TABLE +
+//                    " (" + CLIENT_NAME + " TEXT, " + START_DATE + " TEXT, " + END_DATE + " TEXT, " + ID_CODE + " TEXT, primary key (" +
+//                    ID_CODE + "));");
+            ResultSet resultSet = statement.executeQuery("select * from " + TABLE_CAR);
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(ID_CODE) + " " + resultSet.getString(BRAND) + " " + resultSet.getString(MODEL) + " " +
+                        resultSet.getInt(SEAT_NUMBER) + " " + resultSet.getString(LICENSE_PLATE));
+
+            }
+            statement.close();
+            connection.close();
+
+
+        } catch (SQLException exception) {
+            System.out.println("Error: can't connect to the database " + exception.getMessage());
+            exception.printStackTrace();
+        }
+
+
+        System.out.println("====================================");
+        try {
+            Connection connection = DriverManager.getConnection(CONNECTION_STRING);
+            Statement statement = connection.createStatement();
+
+//            statement.execute("delete from " + RENTAL_TABLE + " where " + CLIENT_NAME + "='hai'");
+//            insertRental(statement, "hai", "31/03/2022", "01/04/2022", "C03");
+//            insertRental(statement, "joe", "23/12/2020", "10/03/2021", "C13");
+//            insertRental(statement, "james", "06/01/2021", "31/02/2022", "C21");
+
+            ResultSet resultSet = statement.executeQuery("select * from " + RENTAL_TABLE);
+            statement.execute("select " + ID_CODE + " from " + TABLE_CAR + " inner join " +
+                    RENTAL_TABLE + " on " + TABLE_CAR + "." + ID_CODE + " = " + RENTAL_TABLE + "." + ID_CODE1 + ";");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(CLIENT_NAME) + " " + resultSet.getString(START_DATE) + " "
+                        + resultSet.getString(END_DATE) + " " + resultSet.getString(ID_CODE));
+            }
+            statement.close();
+            connection.close();
+        } catch (SQLException exception) {
+            System.out.println("Error: can't connect to the database " + exception.getMessage());
+            exception.printStackTrace();
+        }
+
+
+    }
 }
