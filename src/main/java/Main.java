0fc88097a763860c7de64ac04822686d34368b6d
buckhi <101989012+buckhi@users.noreply.github.com>
2022-04-03 10:56:55 +0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,21 +2,26 @@ import CarRentalManagement.DataSource;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import javax.xml.crypto.Data;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 import static CarRentalManagement.DataSource.*;
 
 public class Main extends DataSource {
     private static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws FileNotFoundException, SQLException {
+    public static void main(String[] args) throws SQLException, IOException {
         Connection connection = DriverManager.getConnection(CONNECTION_STRING);
         Statement statement = connection.createStatement();
         DataSource dataSource = new DataSource();
@@ -50,7 +55,7 @@ public class Main extends DataSource {
                 resultSet = statement.executeQuery("select * from " + RENTAL_TABLE);
 
                 while (resultSet.next()) {
-                    System.out.println(resultSet.getString(CLIENT_NAME) + " " + resultSet.getString(START_DATE) + " "
+                    System.out.println(resultSet.getInt(ORDER_NUMBER) + " " + resultSet.getString(CLIENT_NAME) + " " + resultSet.getString(START_DATE) + " "
                             + resultSet.getString(END_DATE) + " " + resultSet.getString(ID_CODE1));
                 }
 
@@ -74,9 +79,9 @@ public class Main extends DataSource {
                 break;
 
             case 4:
-                System.out.println("Enter name of client's rental need to delete: ");
+                System.out.println("Enter order of client's rental need to delete: ");
                 String f = scanner.next();
-                String deleteRental = "delete from " + RENTAL_TABLE + " where " + ID_CODE1 + "=" + "'" + f + "';";
+                String deleteRental = "delete from " + RENTAL_TABLE + " where " + ORDER_NUMBER + "='" + f + "';";
                 statement.executeUpdate(deleteRental);
                 break;
             case 5:
@@ -89,17 +94,44 @@ public class Main extends DataSource {
                 System.out.println("Enter ID of the vehicle want to rent: ");
                 String l = scanner.next();
                 String insertNewRental = "insert into " + RENTAL_TABLE + " (" + CLIENT_NAME + ", " +
-                        START_DATE + ", " + END_DATE + ", " + ID_CODE + ") " +
+                        START_DATE + ", " + END_DATE + ", " + ID_CODE1 + ") " +
                         "values('" + g + "', '" + h + "', '" + i + "', '" + l + "')";
                 statement.executeUpdate(insertNewRental);
                 break;
 
             case 6:
+                resultSet = statement.executeQuery("select * from " + TABLE_CAR);
                 HSSFWorkbook wb = new HSSFWorkbook();
                 HSSFSheet sheet = wb.createSheet("Excel Sheet");
                 HSSFRow rowHead = sheet.createRow((short) 0);
+                rowHead.createCell((short) 0).setCellValue("Id");
+                rowHead.createCell((short) 1).setCellValue("Brand");
+                rowHead.createCell((short) 2).setCellValue("Model");
+                rowHead.createCell((short) 3).setCellValue("Seat");
+                rowHead.createCell((short) 4).setCellValue("Plate");
+                int index = 1;
+                while (resultSet.next()) {
+
+                    HSSFRow row = sheet.createRow((short) index);
+                    row.createCell((short) 0).setCellValue(resultSet.getString(1));
+                    row.createCell((short) 1).setCellValue(resultSet.getString(2));
+                    row.createCell((short) 2).setCellValue(resultSet.getString(3));
+                    row.createCell((short) 3).setCellValue(resultSet.getInt(4));
+                    row.createCell((short) 4).setCellValue(resultSet.getString(5));
+                    index++;
+                }
+                FileOutputStream fileOut = new FileOutputStream("src/main/java/exportCar.xls");
+                wb.write(fileOut);
+                fileOut.close();
+                System.out.println("Data is saved in excel file.");
+
                 break;
             case 7:
+                String fileName = "src/main/resources/importCar.xlsx";
+                Vector dataHolder = read(fileName);
+                saveToDatabase(dataHolder);
+
+
                 break;
             default:
                 System.out.println("Invalid option!");
@@ -111,4 +143,68 @@ public class Main extends DataSource {
 
 
     }
+
+    public static Vector read(String fileName) {
+        Vector cellVectorHolder = new Vector();
+        try {
+            FileInputStream myInput = new FileInputStream(fileName);
+            //POIFSFileSystem myFileSystem = new POIFSFileSystem(myInput);
+            XSSFWorkbook myWorkBook = new XSSFWorkbook(myInput);
+            XSSFSheet mySheet = myWorkBook.getSheetAt(0);
+            Iterator rowIter = mySheet.rowIterator();
+            while (rowIter.hasNext()) {
+                XSSFRow myRow = (XSSFRow) rowIter.next();
+                Iterator cellIter = myRow.cellIterator();
+                //Vector cellStoreVector=new Vector();
+                List list = new ArrayList();
+                while (cellIter.hasNext()) {
+                    XSSFCell myCell = (XSSFCell) cellIter.next();
+                    list.add(myCell);
+                }
+                cellVectorHolder.addElement(list);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cellVectorHolder;
+    }
+
+    private static void saveToDatabase(Vector dataHolder) {
+        String ID = "";
+        String brand = "";
+        String model = "";
+        String seat = "";
+        String plate = "";
+        System.out.println(dataHolder);
+
+        for (Iterator iterator = dataHolder.iterator(); iterator.hasNext(); ) {
+            List list = (List) iterator.next();
+            ID = list.get(0).toString();
+            brand = list.get(1).toString();
+            model = list.get(2).toString();
+            seat = list.get(3).toString();
+            plate = list.get(4).toString();
+
+            try {
+                Connection connection = DriverManager.getConnection(CONNECTION_STRING);
+                PreparedStatement statement = connection.prepareStatement("INSERT INTO " + TABLE_CAR + " VALUES(?,?,?,?,?)");
+                statement.setString(1, ID);
+                statement.setString(2, brand);
+                statement.setString(3, model);
+                statement.setString(4, seat);
+                statement.setString(5, plate);
+                statement.executeUpdate();
+
+                System.out.println("Data is inserted");
+                statement.close();
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+
+            }
+        }
+    }
 }
+
+
+
